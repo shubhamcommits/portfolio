1bc3c3f4a8740c355cd0a3bb1af95728c1ca3aa9
Shubham Singh <shubham1.singh@africa.airtel.com>
2024-05-25 22:12:16 +0530

    Update the hero.tsx with the about me section

--- src/app/components/hero.tsx
+++ src/app/components/hero.tsx
@@ -1,31 +1,113 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import { motion } from "framer-motion";
 import { LampContainer } from "./ui/lamp";
- 
-// Words array
-const words = `I am a versatile and passionate professional with over 7 years of experience in engineering, entrepreneurship, and research. My journey spans core software development, platform engineering, DevOps/DevSecOps, system architecture, and more. I thrive in solving complex problems and driving innovation across diverse industries.`;
+import { HoveredLink, Menu, MenuItem, ProductItem } from "./ui/navbar-menu";
+import { HeroHighlight, Highlight } from "./ui/hero-highlight";
+import { cn } from "@/utils/cn";
 
 export function Hero() {
   return (
-    <div className="w-full rounded-md flex md:items-center md:justify-center antialiased bg-grid-white/[0.02] relative overflow-hidden">
-      <LampContainer>
-        <motion.h1
-          initial={{ opacity: 0.5, y: 100 }}
-          whileInView={{ opacity: 1, y: 0 }}
-          transition={{
-            delay: 0.3,
-            duration: 0.8,
-            ease: "easeInOut",
-          }}
-          className="mt-8 bg-gradient-to-br from-slate-300 to-slate-500 py-4 bg-clip-text text-center text-3xl font-medium tracking-tight text-transparent md:text-6xl"
-        >
-           Hi, I&apos;m Shubham Singh <br /> - Engineer. Entrepreneur. Author -
-        </motion.h1>
-      </LampContainer>
-      {/* <p className="mt-4 font-normal text-base text-neutral-300 max-w-lg text-center mx-auto">
-          I am a versatile and passionate professional with over 7 years of experience in engineering, entrepreneurship, and research. My journey spans core software development, platform engineering, DevOps/DevSecOps, system architecture, and more. I thrive in solving complex problems and driving innovation across diverse industries.
-        </p> */}
+    <div className="bg-black">
+      <section id="hero" className="w-full rounded-md antialiased bg-grid-white/[0.02] relative overflow-hidden">
+        <div className="relative w-full flex items-center justify-center">
+          <Navbar className="top-2" />
+        </div>
+        <LampContainer>
+          <motion.h1
+            initial={{ opacity: 0.5, y: 100 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{
+              delay: 0.3,
+              duration: 0.8,
+              ease: "easeInOut",
+            }}
+            className="mt-8 bg-gradient-to-br from-slate-300 to-slate-500 py-4 bg-clip-text text-center text-3xl font-medium tracking-tight text-transparent md:text-6xl"
+          >
+            Hi, I&apos;m Shubham Singh <br /> - Engineer. Entrepreneur. Author -
+          </motion.h1>
+        </LampContainer>
+      </section>
+      <section id="about">
+        <HeroHighlight>
+          <motion.h1
+            initial={{
+              opacity: 0,
+              y: 20,
+            }}
+            animate={{
+              opacity: 1,
+              y: [20, -5, 0],
+            }}
+            transition={{
+              duration: 0.5,
+              ease: [0.4, 0.0, 0.2, 1],
+            }}
+            className="text-2xl px-4 md:text-2xl lg:text-2xl font-semibold dark:text-white max-w-4xl leading-relaxed lg:leading-snug text-center mx-auto "
+          >
+ I am a versatile and passionate professional with over 7 years of experience in engineering, entrepreneurship, and research. My journey spans {" "}
+ <Highlight className="text-gray-800">
+              core software development, platform engineering, DevOps/DevSecOps, system architecture, and more.
+            </Highlight> I thrive in solving complex problems and driving innovation across diverse industries.
+          </motion.h1>
+        </HeroHighlight>
+      </section>
+    </div>
+  );
+}
+
+function Navbar({ className }: { className?: string }) {
+  const [active, setActive] = useState<string | null>(null);
+  return (
+    <div
+      className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)}
+    >
+      <Menu setActive={setActive}>
+      <MenuItem setActive={setActive} active={active} item="About">
+          <div className="flex flex-col space-y-4 text-sm">
+            <HoveredLink href="#about">Myself</HoveredLink>
+            <HoveredLink href="/individual">Individual</HoveredLink>
+            <HoveredLink href="/team">Team</HoveredLink>
+            <HoveredLink href="/enterprise">Enterprise</HoveredLink>
+          </div>
+        </MenuItem>
+        <MenuItem setActive={setActive} active={active} item="Services">
+          <div className="flex flex-col space-y-4 text-sm">
+            <HoveredLink href="/web-dev">Web Development</HoveredLink>
+            <HoveredLink href="/interface-design">Interface Design</HoveredLink>
+            <HoveredLink href="/seo">Search Engine Optimization</HoveredLink>
+            <HoveredLink href="/branding">Branding</HoveredLink>
+          </div>
+        </MenuItem>
+        <MenuItem setActive={setActive} active={active} item="Recent projects">
+          <div className="  text-sm grid grid-cols-2 gap-10 p-4">
+            <ProductItem
+              title="Algochurn"
+              href="https://algochurn.com"
+              src="https://assets.aceternity.com/demos/algochurn.webp"
+              description="Prepare for tech interviews like never before."
+            />
+            <ProductItem
+              title="Tailwind Master Kit"
+              href="https://tailwindmasterkit.com"
+              src="https://assets.aceternity.com/demos/tailwindmasterkit.webp"
+              description="Production ready Tailwind css components for your next project"
+            />
+            <ProductItem
+              title="Moonbeam"
+              href="https://gomoonbeam.com"
+              src="https://assets.aceternity.com/demos/Screenshot+2024-02-21+at+11.51.31%E2%80%AFPM.png"
+              description="Never write from scratch again. Go from idea to blog in minutes."
+            />
+            <ProductItem
+              title="Rogue"
+              href="https://userogue.com"
+              src="https://assets.aceternity.com/demos/Screenshot+2024-02-21+at+11.47.07%E2%80%AFPM.png"
+              description="Respond to government RFPs, RFIs and RFQs 10x faster using AI"
+            />
+          </div>
+        </MenuItem>
+      </Menu>
     </div>
   );
 }
