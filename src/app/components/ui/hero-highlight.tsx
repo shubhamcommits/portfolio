bb2ce0d17f6e48142ba1a8de57e67b77a0e2a7d9
Shubham Singh <shubham1.singh@africa.airtel.com>
2024-05-26 21:26:27 +0530

    Add hero highlights component

--- src/app/components/ui/hero-highlight.tsx
+++ src/app/components/ui/hero-highlight.tsx
@@ -29,7 +29,7 @@ export const HeroHighlight = ({
   return (
     <div
       className={cn(
-        "relative h-[40rem] flex items-center dark:bg-black justify-center w-full group",
+        "relative h-[50rem] max-h-screen flex items-center dark:bg-black justify-center w-full group",
         containerClassName
       )}
       onMouseMove={handleMouseMove}
@@ -86,7 +86,7 @@ export const Highlight = ({
         display: "inline",
       }}
       className={cn(
-        `relative inline-block pb-1 px-1 rounded-lg bg-gradient-to-r from-indigo-300 to-purple-300 dark:from-indigo-500 dark:to-purple-500`,
+        `relative inline-block pb-1 px-1 rounded-lg bg-gradient-to-r from-indigo-500 to-purple-500 dark:from-indigo-500 dark:to-purple-500`,
         className
       )}
     >
